129e060ee5af35ee804320baf3deb2f5d428b179
dagofonseca <daafonsecara@unal.edu.co>
2018-03-18 08:30:16 -0500

    sessions controller fixed

--- user_ms/app/controllers/sessions_controller.rb
+++ user_ms/app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def show
     current_user = User.where(authentication_token: params[:authentication_token]).first
   	if current_user
-  		render json: current_user.as_json(except: [:id, :password]), status: :aacepted
+  		render json: current_user.as_json(except: [:id, :password]), status: :acepted
   	else
   		head(:unauthorized)
   	end
